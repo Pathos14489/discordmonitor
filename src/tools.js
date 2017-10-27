d4fd6bc278d8c3ff8d5d405e0d048cb31b960aba
Pathos14489 <shinigamiking14489@gmail.com>
2017-10-26 23:24:01 -0400

    Added a new output command to replace console.log and to streamline error bouncing to the main ul. And added the tokenMonitor createBot function here instead of it having it's own .js file.

--- src/tools.js
+++ src/tools.js
@@ -1,4 +1,33 @@
 module.exports = {
+    output: function(argument, type){ 
+    tools.scrollToBottom("chat-log");
+    
+    const li = document.createElement('li');
+    li.className = "list-group-item message";
+    message = document.createTextNode(argument);
+    const image = document.createElement('img');
+    image.style="width:50px;height:50px;"
+
+    if(type == "error"){
+        li.style = "background-color:#550000;";
+        image.src="./../../assets/imgs/WARNING.png"
+    }else if(type == "log"){
+        li.style = "background-color:#808080;";
+    }else if(typeof type != "string"){
+        type = "log"
+        li.style = "background-color:#808080;";
+    }else{
+        li.style = "background-color:#550000;";
+        image.src="./../../assets/imgs/WARNING.png"
+        message = document.createTextNode("ERROR: OUTPUT TYPE: "+ type +"IS NOT VALID!");
+    }
+    
+    if(type == "error"){
+        li.appendChild(image)
+    }
+    li.appendChild(message);
+    ul.appendChild(li);
+    },
     sleep: function (milliseconds) {
         var start = new Date().getTime();
         for (var i = 0; i < 1e7; i++) {
@@ -15,18 +44,28 @@ module.exports = {
         var div = document.getElementById(id);
         div.scrollTop = 0;
     },
-        //Require jQuery
+    //Require jQuery
     scrollSmoothToBottom: function (id) {
         var div = document.getElementById(id);
         $('#' + id).animate({
             scrollTop: div.scrollHeight - div.clientHeight
         }, 500);
     },
-        //Require jQuery
+    //Require jQuery
     scrollSmoothToTop: function (id) {
         var div = document.getElementById(id);
         $('#' + id).animate({
             scrollTop: 0
         }, 500);
+    },
+    createBot: function (token){
+        document.getElementById("loaded").textContent = "const Discord = require('discord.js');const bot = new Discord.Client();"
+        tools.output("Creating bot...", "log")
+        bot.login(token)   
+            .then(function(){
+                tools.output("Token Monitor Active — Bot logged in!", "log")
+                document.getElementById("name").textContent = bot.user.username
+                monitorDiscord.monitor("Token Monitor")
+            });
     },
 };
